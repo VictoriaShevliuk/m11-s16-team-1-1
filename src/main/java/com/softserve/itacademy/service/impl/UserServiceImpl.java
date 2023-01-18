e42fb5b68c09fa776c7211b67d442e0b786b04b3
IgorSVat <igorsvbrody@gmail.com>
2023-01-18 23:27:47 +0200

    UserServiceImpl changed method create to save password with BCrypt and modified name of parameter from role to user

--- src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.repository.UserRepository;
 import com.softserve.itacademy.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -15,14 +16,18 @@ public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    public User create(User role) {
-        if (role != null) {
-            return userRepository.save(role);
+    public User create(User user) {
+        if (user != null) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            return userRepository.save(user);
         }
         throw new NullEntityReferenceException("User cannot be 'null'");
     }
