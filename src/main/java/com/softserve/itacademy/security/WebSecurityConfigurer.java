a871bf3c3eb0086679d1bba7965700e239925040
IgorSVat <igorsvbrody@gmail.com>
2023-01-18 22:39:46 +0200

    WebSecurityConfigurer added PasswordEncoder: BCrypt, override method configure(AuthenticationManagerBuilder) to UserDetailsService with BCrypt

--- src/main/java/com/softserve/itacademy/security/WebSecurityConfigurer.java
+++ src/main/java/com/softserve/itacademy/security/WebSecurityConfigurer.java
@@ -1,11 +1,16 @@
 package com.softserve.itacademy.security;
 
+import com.softserve.itacademy.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -14,10 +19,14 @@ import javax.sql.DataSource;
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private DataSource dataSource;
+    private UserDetailsServiceImpl userDetailsService;
 
     static final String LOGIN_PAGE = "/form-login";
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,28 +42,26 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .permitAll()
                     .and()
                 .logout()
-                .logoutUrl("/perform-logout")
+                //.logoutUrl("/perform-logout")
                 //.logoutUrl(LOGIN_PAGE + "?logout=true")
-                .logoutSuccessUrl(LOGIN_PAGE + "(logout=true)")
-                .deleteCookies("JSESSIONID");
+                //.logoutSuccessUrl(LOGIN_PAGE + "(logout=true)")
+                //.logoutSuccessUrl(LOGIN_PAGE + "?logout")
+                .deleteCookies("JSESSIONID")
+                    .and()
+                            .csrf().disable()
+                .userDetailsService(userDetailsService)
+                ;
+
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource)
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 
-        auth.inMemoryAuthentication()
-                .withUser("mike@mail.com")
-                .password("{noop}123")
-                .roles("ADMIN");
-
-        auth.inMemoryAuthentication()
-                .withUser("nick@mail.com")
-                .password("{noop}123")
-                .roles("USER");
-
-
+//        auth.inMemoryAuthentication()
+//                .withUser("mike@mail.com")
+//                .password("{noop}123")
+//                .roles("ADMIN");
 
     }
 }
